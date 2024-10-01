1435bf42834a578dcba830f6ca21c0a8e73aefa6
Oliver Griffin <olivergriffin2991@gmail.com>
2024-10-01 15:47:32 +0000

    initial commit

--- destructuring.js
+++ destructuring.js
@@ -3,10 +3,58 @@
  * command node destructuring.js in the terminal
  */
 
-// Destructuring arrays
+// ---------------------------------Destructuring arrays
+let ages = [30, 26, 27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [john, mary, joe] = ages;
 
-// Destructuring objects
+console.log(john, mary, joe);
 
-// Destructuring subsets
+// ---------------------------------Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia);
 
-// Using rest parameter syntax
+// ---------------------------------Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+
+let [, , maryNative, marySecondary] = languages; /* the commas before skip elements within the array,
+one comma for each element skipped*/
+console.log(maryNative, marySecondary);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "german",
+    fourthLanguage: "japanese",
+};
+
+let {firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage, thirdLanguage);
+
+// ---------------------------------Using rest parameter syntax
+
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favourite, secondFavourite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavourite);
+console.log(others);
+
+let favouriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak",
+};
+let {brian, anna, ...rest} = favouriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
